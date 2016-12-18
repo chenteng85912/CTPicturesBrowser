e5d6bdb199d90f35f85efacafca2e0379f879257
薇凤的腾 <1410105@qq.com>
2016-12-19 01:06:32 +0800

    Update CTImagePreviewViewController.h

--- CTPicturesShow/ShowPIcture/CTImagePreviewViewController.h
+++ CTPicturesShow/ShowPIcture/CTImagePreviewViewController.h
@@ -1,9 +1,7 @@
 //
 //  ImagePreviewViewController.h
-//  TYKYTwoLearnOneDo
 //
-//  Created by Apple on 16/7/22.
-//  Copyright © 2016年 深圳太极云软技术股份有限公司. All rights reserved.
+//  Created by teng on 16/7/22.
 //
 
 #import <UIKit/UIKit.h>
